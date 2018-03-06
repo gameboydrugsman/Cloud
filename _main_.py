3df70ff1ee4ea6ccf1f798e783ee598458f7ec5a
gameboydrugsman <j.j.dejosselindejong@gmail.com>
2018-03-06 21:51:01 +0100

    Update _main_.py

--- _main_.py
+++ _main_.py
@@ -6,7 +6,7 @@ from pymongo import errors, MongoClient
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
-ipaddress = '10.0.1.18'
+ipaddress = '10.0.1.12'
 port = 27017
 client = MongoClient(ipaddress, port)
 db = client.gebruikers
