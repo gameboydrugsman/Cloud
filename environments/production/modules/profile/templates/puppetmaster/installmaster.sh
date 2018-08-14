90fd650947569950fdb4dcb9aa27997a82f215ef
Joris <j.j.dejosselindejong@gmail.com>
2018-08-14 20:58:45 +0200

    update files

--- environments/production/modules/profile/templates/puppetmaster/installmaster.sh
+++ environments/production/modules/profile/templates/puppetmaster/installmaster.sh
@@ -1,4 +1,6 @@
 sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm ; \
 sudo yum update -y ; \
-sudo yum -y install puppetserver && \
-sudo git 
+sudo yum -y install puppetserver git && \
+sudo rm -rf /etc/puppetlabs/code && \
+cd /etc/puppetlabs && \
+sudo git clone -b puppet https://github.com/jorisdejosselin/Cloud.git code
